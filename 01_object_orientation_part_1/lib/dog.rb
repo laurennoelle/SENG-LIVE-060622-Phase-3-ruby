55c572eff477c6b41e17afc266c97bd1b20f3ac4
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-07-22 10:08:55 -0600

    working code lecture 1

--- 01_object_orientation_part_1/lib/dog.rb
+++ 01_object_orientation_part_1/lib/dog.rb
@@ -1,13 +1,25 @@
 class Dog
   # ✅ we should be able to create dogs with a name, age, breed, and image_url
+  attr_accessor :name, :age, :image_url, :breed, :last_fed_at, :last_walked
 
-  
+  def initialize(attributes={})
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end 
 
   # ✅ we want to be able to access a dog's name, age, breed, image_url, last_fed_at and last_walked at times
 
-
+  
 
   # ✅ we want to be able to print details about a dog (this time including the last walked at and last fed at times)
+  def print
+    puts
+    puts self.name
+    puts "    Age: #{self.age}"
+    puts "    Breed: #{self.breed}"
+    puts "    Image_url: #{self.image_url}"
+  end
 
 
 end
